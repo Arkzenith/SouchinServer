e39712f88331a05d9162528872909a467d16e109
Arkzenith <resist@yeah.net>
2016-06-03 19:17:53 +0800

    project initialized

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+#include "SCServer.h"
+
 using namespace std;
 
 
-int main() {
+int main(int argc,char const ** argv) {
 
 
+    SCServer *server =  new SCServer();
+
+    server->setAcception(new ThreadAcception());
+
+    server->start();
 
     return 0;
 }
